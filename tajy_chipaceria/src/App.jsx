f06e43054c45120d55cf81984b22039f5d53bb1a
Ulises <uliisesrodriguez809@gmail.com>
2023-12-05 20:37:09 -0300

    guardar cambios

--- tajy_chipaceria/src/App.jsx
+++ tajy_chipaceria/src/App.jsx
@@ -9,6 +9,7 @@ function App() {
       <h4>veo si se me borra el h4</h4>
       <h3>ahora hacer el pull desde mi rama</h3>
       <h5>creo q funciona :3</h5>
+      <h6>gurdando cambios</h6>
     </>
   )
 }
