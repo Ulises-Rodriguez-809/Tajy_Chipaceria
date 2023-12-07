ba029a87a3dc380f1bd0af96cb78a94549d10286
Ulises <uliisesrodriguez809@gmail.com>
2023-12-07 11:54:49 -0300

    prueba

--- tajy_chipaceria/src/App.jsx
+++ tajy_chipaceria/src/App.jsx
@@ -4,12 +4,7 @@ function App() {
 
   return (
     <>
-      <h1>Tajy Chipaceria</h1>
-      <h2>probando las ramasfsd</h2>
-      <h4>veo si se me borra el h4</h4>
-      <h3>ahora hacer el pull desde mi rama</h3>
-      <h5>creo q funciona :3</h5>
-      <h6>gurdando cambios</h6>
+
     </>
   )
 }
