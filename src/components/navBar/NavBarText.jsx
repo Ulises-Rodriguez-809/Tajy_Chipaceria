9f15916bb634458df9994261ca8a490879ca5821
Ulises <uliisesrodriguez809@gmail.com>
2024-01-20 21:32:00 -0300

    componente con nombre en minuscula

--- src/components/navBar/NavBarText.jsx
+++ src/components/navBar/NavBarText.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 export const NavBarText = () => {
     return (
-        <div className='navBar-text fontStyle'>
-            <p>
-                Descubre la auténtica experiencia de Tajy Chipacería, donde la calidez y la tradición se fusionan en cada bocado. Nuestros chipa calentitos, ofrecen una experiencia única que no querrás perderte.  ¡Haz de tu día algo especial con Tajy Chipacería!
-            </p>
-        </div>
+        <>
+            <div className='navBar-text fontStyle'>
+                <p>
+                    Descubre la auténtica experiencia de Tajy Chipacería, donde la calidez y la tradición se fusionan en cada bocado. Nuestros chipa calentitos, ofrecen una experiencia única que no querrás perderte.  ¡Haz de tu día algo especial con Tajy Chipacería!
+                </p>
+            </div>
+        </>
     )
 }
--- src/pages/NavBarContainer.jsx
+++ src/pages/NavBarContainer.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { NavBar } from '../components/navBar/NavBar'
-import { NavBarText } from '../components/navBar/navBarText'
 import { NavBarTitle } from '../components/navBar/NavBarTitle'
 import { NavBarImg } from '../components/navBar/NavBarImg'
 import { NavBarButton } from '../components/navBar/NavBarButton'
+import { NavBarText } from '../components/navBar/navBarText'
 
 export const NavBarContainer = () => {
 
